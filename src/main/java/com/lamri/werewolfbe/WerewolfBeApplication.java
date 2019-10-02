eb57620b688edbce44021b43486a8e3ffce3f405
Hung Thanh Nguyen <Hung.Thanh.Nguyen2@mgm-tp.com>
2019-10-02 21:37:31 +0700

    ww-19 Push be to gitHub

---
+++ src/main/java/com/lamri/werewolfbe/WerewolfBeApplication.java
@@ -0,0 +1,13 @@
+package com.lamri.werewolfbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WerewolfBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WerewolfBeApplication.class, args);
+	}
+
+}
