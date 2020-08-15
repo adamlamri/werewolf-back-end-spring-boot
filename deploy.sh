299da0782850efa8bd7a5b74a21add805c031afa
Hung Thanh Nguyen <Hung.Thanh.Nguyen2@mgm-tp.com>
2020-08-16 00:07:25 +0700

    Dockerize run -d

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ echo 'docker image build -t spring-boot'
 docker image build -t spring-boot .
 
 echo 'docker container run'
-docker container run -p 8080:8080 --name spring-boot-cv-container spring-boot
+docker container run -d -p 8080:8080 --name spring-boot-cv-container spring-boot
